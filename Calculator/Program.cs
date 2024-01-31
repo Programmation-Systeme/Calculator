d6eb636f0a97b148f5db86d0f35475e43588feeb
w.mesfioui <w.mesfioui@optimum-automotive.com>
2024-01-31 10:22:52 +0100

    feat: additon

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,6 +7,22 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            addition();
+        }
+
+        static void addition()
+        {
+            int firstNumber, secondNumber, result;
+
+            Console.WriteLine("Enter your first number:");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number:");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            result = firstNumber + secondNumber;
+            Console.WriteLine("Our Addition Result: " + result);
         }
     }
 }
