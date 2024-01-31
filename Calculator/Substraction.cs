367d062fdc10bb0e4fe0ac4e6e5a206fe4481d01
EthanTESTA <testaethan@gmail.com>
2024-01-31 11:13:46 +0100

    ajout de la classe de soustraction

---
+++ Calculator/Substraction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Substraction
+    {
+        /// <summary>
+        /// Method to substract one number to another.
+        /// </summary>
+        /// <param name="number1">First number</param>
+        /// <param name="number2">Second number</param>
+        /// <returns>The result of the substraction</returns>
+        public int Substract(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+    }
+}
