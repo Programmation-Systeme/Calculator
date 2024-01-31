6e6a8e515c87a83ce88b311d9526e672d7db4edf
Gaetan Chatard <gaetan.chatard@ViaCesi.fr>
2024-01-31 10:43:13 +0100

    add multiplication function

--- Calculator/obj/Debug/net5.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net5.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5401a23059601adbf893b68cb1e5e8e324a70dec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
